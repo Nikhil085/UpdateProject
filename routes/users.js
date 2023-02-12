6f41fd891561017831a93fdf418f84ffefdb9626
Nikhil <mahajannikhil085@gmail.com>
2023-02-12 10:40:54 +0530

    Initial Commit

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var plm = require('passport-local-mongoose');
+
+
+mongoose.connect('mongodb://localhost/blogapp1');
+
+var userSchema = mongoose.Schema({
+  name:'String',
+  username:'String',
+  profileImage:{
+    type:'String',
+    default:'./images/uploads/default.jpg'
+  },
+  posts:[
+    { type: mongoose.Schema.Types.ObjectId,
+    ref:'posts'}
+  ],
+  email:'String',
+  password:'String'
+})
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('users', userSchema);
+
+ 
